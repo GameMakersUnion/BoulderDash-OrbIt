08ac703c945dab8ac2b1f20b276c5e088a26e238
MHDante <mhdcj@hotmail.com>
2014-05-15 17:56:37 -0400

    Moved collision code out of the room.

--- OrbItProcs/OrbItProcs/Room.cs
+++ OrbItProcs/OrbItProcs/Room.cs
@@ -16,13 +16,7 @@ namespace OrbItProcs {
     {
         //////Collision
         //Move to own class Later
-        
-        public int colIterations { get; set; }
-        public HashSet<Collider> CollisionSetCircle { get; set; }
-        public HashSet<Collider> CollisionSetPolygon { get; set; }
-        private List<Manifold> contacts = new List<Manifold>();
-        public GridSystem gridsystemCollision { get; set; }
-
+        public CollisionManager collisionManager;
 
         ////Room
         //consts
@@ -71,7 +65,6 @@ namespace OrbItProcs {
 
         //Events
         public event EventHandler AfterIteration;
-        Action<Collider, Collider> collideAction;
 
         public Room(OrbIt game, int worldWidth, int worldHeight, bool Groups = true)
         {
@@ -81,46 +74,16 @@ namespace OrbItProcs {
 
             this.worldWidth = worldWidth;
             this.worldHeight = worldHeight;
-            CollisionSetCircle = new HashSet<Collider>();
-            CollisionSetPolygon = new HashSet<Collider>();
-            colIterations = 1;
+
+            
             
             
             scheduler = new Scheduler();
             borderColor = Color.DarkGray;
-            collideAction = (c1, c2) =>
-            {
-                if (c1.parent == c2.parent) return;
-                if (c1 is Body)
-                {
-                    Body b = (Body)c1;
-                    
-                    if (gridsystemCollision.alreadyVisited.Contains(c2)) return;
-                    if (c2 is Body)
-                    {
-                        Body bb = (Body)c2;
-                        //if (!b.exclusionList.Contains(bb)) 
-                            b.CheckCollisionBody(bb);
-                    }
-                    else
-                    {
-                        b.CheckCollisionCollider(c2);
-                    }
-                }
-                else
-                {
-                    if (gridsystemCollision.alreadyVisited.Contains(c2)) return;
-                    if (c2 is Body)
-                    {
-                        Body bb = (Body)c2;
-                        //if (!c1.exclusionList.Contains(bb)) 
-                            c1.CheckCollisionBody(bb);
-                    }
-                }
-            };
+            
             // grid System
             gridsystemAffect = new GridSystem(this, 40, new Vector2(0, worldHeight - OrbIt.ScreenHeight), worldWidth, OrbIt.ScreenHeight);
-            gridsystemCollision = new GridSystem(this, gridsystemAffect.cellsX, new Vector2(0, worldHeight - OrbIt.ScreenHeight), worldWidth, OrbIt.ScreenHeight);
+            collisionManager = new CollisionManager(this);
             //gridsystemAffect = new GridSystem(this, 40, new Vector2(0, worldHeight - OrbIt.Height), worldWidth, OrbIt.Height);
             level = new Level(this, 40, 40, gridsystemAffect.cellWidth, gridsystemAffect.cellHeight);
             roomRenderTarget = new RenderTarget2D(game.GraphicsDevice, OrbIt.ScreenWidth, OrbIt.ScreenHeight);
@@ -233,20 +196,7 @@ namespace OrbItProcs {
             }
         }
         
-        public void AddCollider(Collider collider)
-        {
-            if (collider.shape is Circle)
-                CollisionSetCircle.Add(collider);
-            else if (collider.shape is Polygon)
-                CollisionSetPolygon.Add(collider);
-        }
-        public void RemoveCollider(Collider collider)
-        {
-            if (CollisionSetCircle.Contains(collider))
-                CollisionSetCircle.Remove(collider);
-            if (CollisionSetPolygon.Contains(collider))
-                CollisionSetPolygon.Remove(collider);
-        }
+        
         public int affectAlgorithm = 2;
         public bool ColorNodesInReach = false;
 
@@ -307,8 +257,7 @@ namespace OrbItProcs {
                 }
             }
 
-            UpdateCollision();
-            if (contacts.Count > 0) contacts = new List<Manifold>();
+            collisionManager.Update();
 
             foreach(Node n in masterGroup.fullSet.ToList())
             {
@@ -355,102 +304,7 @@ namespace OrbItProcs {
             }
         }
 
-        static int algorithm = 7;
-        private void UpdateCollision()
-        {
-            Testing.modInc();
-            if (algorithm >= 5)
-            {
-                gridsystemCollision.clearBuckets();
-                foreach (var c in CollisionSetCircle) //.ToList()
-                {
-                    //if (skipOutsideGrid && (c.pos.Y < gridsystemCollision.position.Y || c.pos.Y > gridsystemCollision.position.Y + gridsystemCollision.gridHeight)) continue;
 
-                    //if (ColorNodesInReach) c.parent.body.color = Color.White;
-                    if (!c.parent.active) continue;
-                    gridsystemCollision.insertToBuckets(c);
-                }
-            }
-
-            Testing.PrintTimer("insertion");//oh, mama.
-
-            gridsystemCollision.alreadyVisited = new HashSet<Collider>();
-
-            foreach (var c in CollisionSetPolygon.ToList())
-            {
-                if (c.parent.active)
-                {
-                    gridsystemCollision.alreadyVisited.Add(c);
-                    int reach = (int)c.radius * 2;
-                    if (c.shape is Circle)//todo fix the fact that circles are in the polygons list
-                    {
-                        reach = (int)(c.shape as Polygon).polyReach;
-                    }
-                    foreach (var otherCol in CollisionSetPolygon.ToList())
-                    {
-                        collideAction(c, otherCol);
-                    }
-                    foreach (var otherCol in CollisionSetCircle.ToList())
-                    {
-                        collideAction(c, otherCol);
-                    }
-                }
-            }
-
-            foreach (var c in CollisionSetCircle.ToList()) //.ToList() 
-            {
-                //if (skipOutsideGrid && (c.pos.Y < gridsystemCollision.position.Y || c.pos.Y > gridsystemCollision.position.Y + gridsystemCollision.gridHeight)) continue;
-                if (c.parent.active)
-                {
-                    gridsystemCollision.alreadyVisited.Add(c);
-                    int reach = (int)c.radius * 2;
-                    if (c.shape is Polygon)
-                    {
-                        reach = (int)(c.shape as Polygon).polyReach; //shouldnt have a polygon in the circle list
-                        foreach (var otherCol in CollisionSetCircle.ToList())
-                        {
-                            collideAction(c, otherCol);
-                        }
-                    }
-                    else if (algorithm == 7)
-                    {
-                        gridsystemCollision.retrieveOffsetArraysCollision(c, collideAction, c.radius * 2);
-                    }
-                    //else if (algorithm == 6)
-                    //{
-                    //    gridsystemCollision.retrieveFromAllOffsets(c, reach, collideAction);
-                    //}
-                }
-            }
-            //Testing.PrintTimer("insertion");
-            //Testing.PrintTimer("retrieve");
-            //Testing.PrintTimer("manifolds");
-            //COLLISION
-            foreach (Manifold m in contacts)
-            {
-                m.Initialize();
-            }
-            // \COLLISION
-            //update velocity
-            for (int ii = 0; ii < colIterations; ii++)
-            {
-                foreach (Manifold m in contacts)
-                {
-                    m.ApplyImpulse();
-                    //m.a.parent.SetColor(Color.Green);
-                    //m.b.parent.SetColor(Color.Yellow);
-                }
-            }
-            foreach (Node n in masterGroup.fullSet.ToList())
-            {
-                if (skipOutsideGrid && n.body.pos.isWithin(gridsystemAffect.position, gridsystemAffect.position + new Vector2(gridsystemAffect.gridWidth, gridsystemAffect.gridHeight))) continue;
-                n.movement.IntegrateVelocity();
-                VMath.Set(ref n.body.force, 0, 0);
-                n.body.torque = 0;
-            }
-            foreach (Manifold m in contacts)
-                m.PositionalCorrection();
-        }
         public void GroupSelectDraw() //todo: make this the draw method in groupselect class
         {
             if (processManager.processDict.ContainsKey(typeof(GroupSelect)))
@@ -487,7 +341,7 @@ namespace OrbItProcs {
             camera.drawGrid(linesToDraw, borderColor);
             linesToDraw = new List<Rectangle>();
 
-            if (DrawCollisionGrid) gridsystemCollision.DrawGrid(this, Color.Orange);
+            
             if (DrawAffectGrid) gridsystemAffect.DrawGrid(this, Color.LightBlue);
 
             if (DrawLinks)
@@ -516,10 +370,6 @@ namespace OrbItProcs {
             //camera.RenderAsync();
             //Draw();
             //camera.CatchUp();
-        }
-        public void AddManifold(Manifold m)
-        {
-            contacts.Add(m);
         }
 
         public void MakeWalls(float WallWidth)
@@ -671,12 +521,12 @@ namespace OrbItProcs {
         {
             worldWidth = (int)resizeVect.X;
             worldHeight = (int)resizeVect.Y;
-            int newCellsX = worldWidth / gridsystemCollision.cellWidth;
+            int newCellsX = worldWidth / gridsystemAffect.cellWidth;
             int gridHeight = fillWithGrid ? worldHeight : OrbIt.ScreenHeight;
             gridsystemAffect = new GridSystem(this, newCellsX, new Vector2(0, worldHeight - gridHeight), worldWidth, gridHeight);
             level = new Level(this, newCellsX, newCellsX, gridsystemAffect.cellWidth, gridsystemAffect.cellHeight);
             //roomRenderTarget = new RenderTarget2D(game.GraphicsDevice, worldWidth, worldHeight);
-            gridsystemCollision = new GridSystem(this, newCellsX, new Vector2(0, worldHeight - gridHeight), worldWidth, gridHeight);
+            collisionManager.gridsystemCollision = new GridSystem(this, newCellsX, new Vector2(0, worldHeight - gridHeight), worldWidth, gridHeight);
             fillWithGrid = false;
 
             camera.pos = new Vector2(worldWidth / 2, worldHeight / 2);
